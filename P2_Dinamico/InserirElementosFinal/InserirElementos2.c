8162ed3c4d9d1ec523ce2da179aac80cfe850a68
Laerte Akira <laerteakira@gmail.com>
2025-03-27 11:57:17 -0300

    LDE - Insere Final

---
+++ P2_Dinamico/InserirElementosFinal/InserirElementos2.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct tItem {
+    int chave;
+    struct tItem *proximo;
+};
+
+struct tLista {
+    struct tItem *primeiro;
+};
+
+struct tLista * criarLista() {
+    struct tLista *l = (struct tLista *)malloc(sizeof(struct tLista));
+    if(l != NULL) {
+        l->primeiro = NULL;
+    }
+    return l;
+}
+
+struct tItem * criarItem(int chave) {
+    struct tItem *it = (struct tItem *)malloc(sizeof(struct tItem));
+    if(it != NULL) {
+        it->chave = chave;
+        it->proximo = NULL;
+    }
+    return it;
+}
+
+struct tItem * primeiro(struct tLista *l) {
+    return l->primeiro;
+}
+
+void mostrarLista(struct tLista *l) {
+    struct tItem *it = primeiro(l);
+
+    while(it != NULL) {
+        printf("%d%s", it->chave, it->proximo == NULL ? "\n" : " ");
+        it = it->proximo;
+    }
+}
+
+void insertTail(struct tLista *l, int chave) {
+    struct tItem *anterior = NULL, *atual = primeiro(l);
+    struct tItem *novo = criarItem(chave);
+
+    while(atual != NULL) {
+        anterior = atual;
+        atual = atual->proximo;
+    }
+
+    if(anterior != NULL) {
+        anterior->proximo = novo;
+    } else {
+        l->primeiro = novo;
+    }
+}
+
+int main() {
+    int nListas, nElem, elem;
+    struct tLista *l;
+
+    scanf("%d", &nListas);
+
+    int i;
+    for(i = 0; i < nListas; i++) {
+        scanf("%d", &nElem);
+
+        l = criarLista();
+
+        int j;
+        for(j = 0; j < nElem; j++) {
+            scanf("%d", &elem);
+            insertTail(l, elem);
+        }
+
+        mostrarLista(l);
+    }
+
+    return 0;
+}
