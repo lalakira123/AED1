9d95877c28ce86d09e1449cf063e2102eaa43a66
Laerte Akira <laerteakira@gmail.com>
2025-04-27 16:21:10 -0300

    Remover Antecessor/Sucessor

---
+++ P3/RemoverAntecessor/RemoverAntecessor.c
@@ -0,0 +1,211 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct tNo {
+    int chave;
+    struct tNo *pai;
+    struct tNo *esquerda;
+    struct tNo *direita;
+};
+
+struct tArvore {
+    struct tNo *raiz;
+};
+
+struct tNo * criarNo(int chave) {
+    struct tNo *no = (struct tNo *)malloc(sizeof(struct tNo));
+    if(no != NULL) {
+        no->chave = chave;
+        no->pai = NULL;
+        no->esquerda = NULL;
+        no->direita = NULL;
+    }
+    return no;
+}
+
+struct tArvore * criarArvore() {
+    struct tArvore *t = (struct tArvore *)malloc(sizeof(struct tArvore));
+    if(t != NULL) {
+        t->raiz = NULL;
+    }
+    return t;
+}
+
+struct tNo * raiz(struct tArvore *t) {
+    return t->raiz;
+}
+
+void preOrder(struct tNo *no) {
+    if(no != NULL) {
+        printf("%d\n", no->chave);
+        preOrder(no->esquerda);
+        preOrder(no->direita);
+    }
+}
+
+void inserir(struct tArvore *t, struct tNo *novo) {
+    struct tNo *pai = NULL, *filho = raiz(t);
+
+    while(filho != NULL) {
+        pai = filho;
+        if(filho->chave < novo->chave) {
+            filho = filho->direita;
+        } else if(filho->chave > novo->chave) {
+            filho = filho->esquerda;
+        } else {
+            return;
+        }
+    }
+
+    if(pai != NULL) {
+        novo->pai = pai;
+        if(pai->chave <= novo->chave) {
+            pai->direita = novo;
+        } else {
+            pai->esquerda = novo;
+        }
+    } else {
+        t->raiz = novo;
+    }
+}
+
+struct tNo * antecessor(struct tNo *no) {
+    struct tNo *pai = no->pai;
+    
+    no = no->esquerda;
+    while(no != NULL) {
+        pai = no;
+        no = no->direita;
+    }
+
+    return pai;
+}
+
+struct tNo * removerAntecessor(struct tNo *no) {
+    struct tNo *pai = no->pai;
+
+    if(pai->esquerda == no) {
+        pai->esquerda = no->esquerda;
+    } else {    
+        pai->direita = no->esquerda;
+    }
+
+    if(no->esquerda != NULL) {
+        no->esquerda->pai = pai;
+    }
+
+    no->esquerda = NULL;
+    no->pai = NULL;
+    return no;
+}
+
+struct tNo * remover(struct tArvore *t, int chave) {
+    struct tNo *pai = NULL, *filho = raiz(t), *subs = NULL;
+
+    while(filho != NULL && filho->chave != chave) {
+        pai = filho;
+        if(filho->chave <= chave) {
+            filho = filho->direita;
+        } else {
+            filho = filho->esquerda;
+        }
+    }
+
+    if(filho != NULL) {
+        if(filho->esquerda == NULL && filho->direita == NULL) {
+            if(pai != NULL) {
+                if(pai->esquerda == filho) {
+                    pai->esquerda = NULL;
+                } else {
+                    pai->direita = NULL;
+                }
+            } else {
+                t->raiz = NULL;
+            }
+        } else if(filho->esquerda != NULL && filho->direita != NULL) {
+            subs = removerAntecessor(antecessor(filho));
+
+            subs->pai = filho->pai;
+            subs->esquerda = filho->esquerda;
+            subs->direita = filho->direita;
+
+            if(pai != NULL) {
+                if(pai->esquerda == filho) {
+                    pai->esquerda = subs;
+                } else {
+                    pai->direita = subs;
+                }
+            } else {    
+                t->raiz = subs;
+            }
+
+            if(filho->esquerda != NULL) {
+                filho->esquerda->pai = subs;
+            }
+
+            if(filho->direita != NULL) {
+                filho->direita->pai = subs;
+            }
+        } else {
+            if(pai != NULL) {
+                if(pai->esquerda == filho) {
+                    if(filho->esquerda != NULL) {
+                        pai->esquerda = filho->esquerda;
+                        filho->esquerda->pai = pai;
+                    } else {
+                        pai->esquerda = filho->direita;
+                        filho->direita->pai = pai;
+                    }
+                } else {
+                    if(filho->esquerda != NULL) {
+                        pai->direita = filho->esquerda;
+                        filho->esquerda->pai = pai;
+                    } else {
+                        pai->direita = filho->direita;
+                        filho->direita->pai = pai;
+                    }
+                }
+            } else {
+                if(filho->esquerda != NULL) {
+                    t->raiz = filho->esquerda;
+                    filho->esquerda->pai = NULL;
+                } else {
+                    t->raiz = filho->direita;
+                    filho->direita->pai = NULL;
+                }
+            }
+        }
+
+        filho->pai = NULL;
+        filho->direita = NULL;
+        filho->esquerda = NULL;
+    }
+
+    return filho;
+}
+
+int main() {
+    int n, valor, nRem;
+    struct tArvore *t = criarArvore();
+    struct tNo *novo;
+
+    scanf("%d", &n);
+
+    int i;
+    for(i = 0; i < n; i++) {
+        scanf("%d", &valor);
+        inserir(t, criarNo(valor));
+    }
+
+    scanf("%d", &nRem);
+
+    int j;
+    for(j = 0; j < nRem; j++) {
+        scanf("%d", &valor);
+        remover(t, valor);
+    }
+
+    preOrder(raiz(t));
+
+    return 0;
+}
